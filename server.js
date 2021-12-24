9613450bf6f3117db519a200591bc18ba488c3de
aimorbar@gmail.com <1Q2w3e4r>
2021-12-24 18:19:37 +0100

    cambio end point para atacar a backend

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
+const session = require('express-session'),
+    cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
     res.sendfile(__dirname + "/dist/index.html");

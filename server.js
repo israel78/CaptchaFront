fee0485508291331a11bce46eba2b94cb602c506
aimorbar@gmail.com <1Q2w3e4r>
2021-12-24 18:22:38 +0100

    Ajustes sesiones express server

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
-
-const session = require('express-session'),
-    cookieParser = require('cookie-parser');
+const session = require('express-session')
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(session({
     secret: 'keyboard cat',
